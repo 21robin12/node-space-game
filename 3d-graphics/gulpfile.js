8ff4ccd3c9e7e38953c42da0b2ccacf5d7222233
Robin Herd RF <robinherd@hotmail.co.uk>
2016-08-18 15:38:10 +0100

    added stuff for typescript

---
+++ 3d-graphics/gulpfile.js
@@ -0,0 +1,22 @@
+﻿// load modules
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+
+gulp.task('scripts', function() {
+    return gulp.src('src/**/*.ts')
+        .pipe(ts({
+            noImplicitAny: true,
+            out: 'output.js'
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('src/**/*.ts', ['scripts']);
+});
+
+gulp.task('default', function () {
+    // can pass many tasks here as an array
+    return gulp.start(['scripts', 'watch']);
+});
+
